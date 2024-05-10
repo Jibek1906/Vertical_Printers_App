b984d1d41b9f3123a1695ea199948e2d29ee7289
zhibek <romanbekovazibek@gmail.com>
2024-05-10 16:30:51 +0600

    changes

--- vertical_printers_app/vertical_printers_project/vertical_printers_project/settings.py
+++ vertical_printers_app/vertical_printers_project/vertical_printers_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'users',
     'rest_framework_simplejwt',
     'printers',
+    'cart',
 ]
 
 REST_FRAMEWORK = {
@@ -138,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'     
+
